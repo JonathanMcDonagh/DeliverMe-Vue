bc66e39b0bbd5dc170c1e8db0899eb72f5cd1e01
jonathanmcdonagh <jonathan.a.mcdonagh@gmail.com>
2020-04-27 19:22:24 +0100

    Minor change for PayPal to work

--- src/components/Forms/JobDetailsForm.vue
+++ src/components/Forms/JobDetailsForm.vue
@@ -1,42 +1,54 @@
 <template>
   <form @submit.prevent="submit">
-      <div class="col-lg-12 col-md-12 col-sm-12 align-center">
+    <div class="col-lg-12 col-md-12 col-sm-12 align-center">
 
-    <div class="form-content align-center">
-      <!-- Displays job status to the users which they cannot edit -->
-      <div class="form-group"  :class="{ 'form-group--error': $v.jobStatus.$error }">
-        <label class="form__label">Accepted By</label>
-        <input class="form__input" style="background-color: #def2f1" disabled v-model.trim="$v.jobStatus.$model"/>
-        <div class="error" v-if="!$v.jobStatus.required">This field is Required</div>
+      <div class="form-content align-center">
+        <!-- Displays job status to the users which they cannot edit -->
+        <div class="form-group"  :class="{ 'form-group--error': $v.jobStatus.$error }">
+          <label class="form__label">Accepted By</label>
+          <input class="form__input" style="background-color: #def2f1" disabled v-model.trim="$v.jobStatus.$model"/>
+          <div class="error" v-if="!$v.jobStatus.required">This field is Required</div>
+        </div>
+
+        <!-- Displays job status to the users which they cannot edit -->
+        <div class="form-group"  :class="{ 'form-group--error': $v.jobMessage.$error }">
+          <label class="form__label">Job Message</label>
+          <input class="form__input" style="background-color: #def2f1" disabled v-model.trim="$v.jobMessage.$model"/>
+          <div class="error" v-if="!$v.jobMessage.required">This field is Required</div>
+        </div>
+
+        <!-- Displays job status to the users which they cannot edit -->
+        <div class="form-group"  :class="{ 'form-group--error': $v.phoneNum.$error }">
+          <label class="form__label">Users Phone Number</label>
+          <input class="form__input" style="background-color: #def2f1" disabled v-model.trim="$v.phoneNum.$model"/>
+          <div class="error" v-if="!$v.phoneNum.required">This field is Required</div>
+        </div>
+
+        <div>
+          <div v-if="!paidFor">
+            <h1>Pay Delivery Fee - €{{ product.price }}</h1>
+            <p>{{ product.description }}</p>
+          </div>
+
+          <div v-if="paidFor">
+            <h1>Thank you, payment made successfully!</h1>
+          </div>
+          <div ref="paypal"></div>
+        </div>
+
+        <p>
+          <button v-if="$store.state.isDriverLoggedIn" class="btn btn-primary btn1" @click="driverJobs">Go Back</button>
+          <button v-else-if="$store.state.isAdminLoggedIn" class="btn btn-primary btn1" @click="adminJobs">Go Back</button>
+          <button v-else class="btn btn-primary btn1" @click="userJobs">Go Back</button>
+
+        </p>
+
+        <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your help</p>
+        <p class="typo__p" v-if="submitStatus === 'ERROR'">Please Fill in the Form Correctly.</p>
+        <p class="typo__p" v-if="submitStatus === 'PENDING'">adding...</p>
       </div>
 
-      <!-- Displays job status to the users which they cannot edit -->
-      <div class="form-group"  :class="{ 'form-group--error': $v.jobMessage.$error }">
-        <label class="form__label">Job Message</label>
-        <input class="form__input" style="background-color: #def2f1" disabled v-model.trim="$v.jobMessage.$model"/>
-        <div class="error" v-if="!$v.jobMessage.required">This field is Required</div>
-      </div>
-
-      <!-- Displays job status to the users which they cannot edit -->
-      <div class="form-group"  :class="{ 'form-group--error': $v.phoneNum.$error }">
-        <label class="form__label">Users Phone Number</label>
-        <input class="form__input" style="background-color: #def2f1" disabled v-model.trim="$v.phoneNum.$model"/>
-        <div class="error" v-if="!$v.phoneNum.required">This field is Required</div>
-      </div>
-
-      <p>
-        <button v-if="$store.state.isDriverLoggedIn" class="btn btn-primary btn1" @click="driverJobs">Go Back</button>
-        <button v-else-if="$store.state.isAdminLoggedIn" class="btn btn-primary btn1" @click="adminJobs">Go Back</button>
-        <button v-else class="btn btn-primary btn1" @click="userJobs">Go Back</button>
-
-      </p>
-
-      <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your help</p>
-      <p class="typo__p" v-if="submitStatus === 'ERROR'">Please Fill in the Form Correctly.</p>
-      <p class="typo__p" v-if="submitStatus === 'PENDING'">adding...</p>
     </div>
-
-      </div>
   </form>
 </template>
 
@@ -45,16 +57,13 @@ import Vue from 'vue'
 import VueForm from 'vueform'
 import Vuelidate from 'vuelidate'
 import { required } from 'vuelidate/lib/validators'
-
 Vue.use(VueForm, {
   inputClasses: {
     valid: 'form-control-success',
     invalid: 'form-control-danger'
   }
 })
-
 Vue.use(Vuelidate)
-
 export default {
   name: 'FormData',
   props: ['jobBtnTitle', 'job'],
@@ -65,6 +74,12 @@ export default {
       jobStatus: this.job.jobStatus,
       jobMessage: this.job.jobMessage,
       phoneNum: this.job.phoneNum,
+      loaded: false,
+      paidFor: false,
+      product: {
+        price: this.job.deliveryFee + 0.50,
+        description: 'Delivery Fee' + '(€' + this.job.deliveryFee + ')' + ' Serivce Charge (€0.50)'
+      },
       submitStatus: null
     }
   },
@@ -79,6 +94,13 @@ export default {
     phoneNum: {
       required
     }
+  },
+  mounted: function () {
+    const script = document.createElement('script')
+    script.src =
+            'https://www.paypal.com/sdk/js?client-id=AXRLrnrAEwQjJRsaarv13yes0IAHizJLG8Vveg80QOARMJTLTx_Iivti-zQEAXWLN85c_VKL5AEzn9t8&currency=EUR'
+    script.addEventListener('load', this.setLoaded)
+    document.body.appendChild(script)
   },
   methods: {
     // Method for submitting
@@ -115,13 +137,41 @@ export default {
       this.fname = this.$store.state.driver.fname
       this.lname = this.$store.state.driver.lname
       this.driverEmail = this.$store.state.driver.email
+    },
+    setLoaded: function () {
+      this.loaded = true
+      window.paypal
+        .Buttons({
+          createOrder: (data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  description: this.product.description,
+                  amount: {
+                    value: this.product.price
+                  }
+                }
+              ]
+            })
+          },
+          onApprove: async (data, actions) => {
+            const order = await actions.order.capture()
+            // eslint-disable-next-line no-unused-expressions
+            this.data
+            this.paidFor = true
+            console.log(order)
+          },
+          onError: err => {
+            console.log(err)
+          }
+        })
+        .render(this.$refs.paypal)
     }
   }
 }
 </script>
 
 <style scoped>
-
   .required-field > label::after {
     content: '*';
     color: red;
@@ -154,7 +204,6 @@ export default {
     padding: 5px 10px;
     width: 100%;
   }
-
   .error {
     border-color: #3AAFA9;
     background: #3AAFA9;
